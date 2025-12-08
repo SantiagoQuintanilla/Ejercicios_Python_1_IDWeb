70f82dbf262a122d01f15c35650ed387c87c7406
SantiagoQuintanilla <squintanilla@unsa.edu.pe>
2025-12-08 14:32:24 -0500

    Finalizado Ejercicio 1

---
+++ Ejercicio01.py
@@ -0,0 +1,9 @@
+print("Temperatura promedio: Fin de semana en Arequipa (en C°)")
+
+viernes=float(input("Ingrese la temperatura del viernes: "))
+sabado=float(input("Ingrese la temperatura del sábado: "))
+domingo=float(input("Ingrese la temperatura del domingo: "))
+
+promedio=(viernes+sabado+domingo)/3
+
+print("La temperatura promedio del fin de semana en Arequipa es:", promedio, "°C")
